a070113c75e8966ffe17f62c2dba970ddc6567a1
Rayan Tighiouart <t.alex.2002@gmail.com>
2023-09-28 23:36:34 -0700

    frontend hosting with firebase

---
+++ frontend/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDywh0zxvc09yY5meNVLNyGtExzziqLS6U",
+  authDomain: "yazify-9a200.firebaseapp.com",
+  projectId: "yazify-9a200",
+  storageBucket: "yazify-9a200.appspot.com",
+  messagingSenderId: "702029227239",
+  appId: "1:702029227239:web:f20276e3a3b5b86d732e42",
+  measurementId: "G-79F9K5LHXW",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
